f2fbaf86cfdd84ef3863ff3f86d132c3ed1b1b30
hemanthkoramutla <101422205+hemanthkoramutla@users.noreply.github.com>
2023-12-15 09:27:28 +0530

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 {   
 //defining an array of integer type   
  
-System.out.println("Elements of array sorted in ascending order: ");  
+System.out.println("This is not a sorting program ");  
 //prints array using the for loop  
  
 }  
